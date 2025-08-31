7a07a65eb8b3d670d74cd614d820172635f25f14
Bhavya-161 <151461585+Bhavya-161@users.noreply.github.com>
2025-09-01 01:35:05 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // important for app/ directory routing
-  },
 };
 
 export default nextConfig;
